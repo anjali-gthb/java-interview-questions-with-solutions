bc9544fdbcd0cddf074a198636995ed8b2cedd6a
anjali <yashika22789@gmail.com>
2022-09-03 22:31:09 +0530

    JAVA

---
+++ java1/solutions_of_intermediate_Questions.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class solutions_of_intermediate_Questions {
+    public static void main(String[] args) {
+
+        // 13. Sum Of N Numbers
+
+        // Scanner input=new Scanner(System.in);
+        // System.out.println("enter the  total number ");
+        // int n=input.nextInt();
+        // int sum=0;
+        // System.out.println("enter the number ");
+        // for(int i=0;i<n;i++){
+        //     int num=input.nextInt();
+        //     sum=sum+num;
+        
+        // }
+        // System.out.println("the total sum of n numbers are "+sum);
+
+
+
+        //  14. Armstrong Number In Java:123=1^3*2^3*3^3 then 123 must be  an armstrong number
+
+            // Scanner input=new Scanner(System.in);
+            // System.out.println("enter the number which u want to check whether it is an armstrong number of not?");
+            // int number=input.nextInt();
+            // int temp=number;
+            // int rem;
+            // int sum=0;
+            // while(temp!=0){
+            //     rem=temp%10;
+            //     temp=temp/10;
+            //     sum=sum+rem*rem*rem;
+            // }
+
+            // if (sum==number) {
+            //     System.out.println("Armstrong number");
+            // }
+            // else{
+            //     System.out.println("Not an armstrong number");
+            // }
+
+
+
+            
+
+        
+
+    }
+}
